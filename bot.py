6e0847609b1282bfba4d896e46653fd0673b2305
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-11-06 22:30:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ import asyncio
 
 # Configurazioni API e HASH
 api_id = '21963510'
-api_hash = 'YOUR_API_HASH'
+api_hash = 'eddfccf6e4ea21255498028e5af25eb1'
+'
 admin_id = 6849853752  # Sostituisci con l'ID autorizzato
 
 clients = []
